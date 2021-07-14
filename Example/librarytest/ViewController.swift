84878939c2f8a69e657f683f05e99ef72381505f
Wanya <wanya@watcha.com>
2021-07-14 16:17:29 +0900

    예제 테스트

--- Example/librarytest/ViewController.swift
+++ Example/librarytest/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import librarytest
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        PrintClass().do()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
---
+++ librarytest/Classes/ReplaceMe.swift
@@ -0,0 +1,8 @@
+
+import Foundation
+
+public class PrintClass: NSObject {
+    public func `do`() {
+        print("print!")
+    }
+}
